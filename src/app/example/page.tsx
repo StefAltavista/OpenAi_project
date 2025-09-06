71eddfb6a504e6fd3abafe39743134dccd1db1eb
AndrewCult <cultraro.andrea@gmail.com>
2025-09-06 11:22:59 +0200

    test commit

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -2,6 +2,7 @@ export default function ChatBot() {
   return (
     <div>
       <h1 className="bg-violet-500 m-10 p-5">This is an example page</h1>
+      <p className="m-10 p-5">This is a test</p>
     </div>
   );
 }
