8e682fb2b19193eb1bfd28b7189f7b7906b42fed
Stef Altavista <stefanomsc@hotmail.it>
2025-09-09 21:18:05 +0200

    Progect Structure

---
+++ src/data/coocks.ts
@@ -0,0 +1,21 @@
+export interface Coock {
+  id: string;
+  name: string;
+  cousine: string;
+  tone: string;
+  communication: string[];
+  errors: string[];
+  avatar: string;
+}
+
+export const coocks: Coock[] = [
+  {
+    id: "1",
+    name: "Mario Rossi",
+    cousine: "Italian",
+    tone: "Friendly",
+    communication: ["messy", "unclear"],
+    errors: ["spelling", "grammar"],
+    avatar: "/avatars/coockMario.png",
+  },
+];
